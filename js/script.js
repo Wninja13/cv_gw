44f8f1d1641917391d938023f7e01054b61279af
GGW <german.weckesser@gmail.com>
2023-10-09 14:26:31 -0300

    Tarjetas 1 intento JS fallido

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    const skills = document.querySelectorAll('.skill-bar .progress');
-    
+    const skillsBars = document.querySelectorAll('.skill-bar .progress');
+    const skills = document.querySelectorAll('.skill');
+
     const checkScroll = function() {
-        skills.forEach(skill => {
-            const position = skill.getBoundingClientRect().top;
+        skillsBars.forEach(skillBar => {
+            const position = skillBar.getBoundingClientRect().top;
             const screenHeight = window.innerHeight;
 
             if (position <= screenHeight) {
-                const width = skill.getAttribute('data-width');
-                skill.style.width = width;
+                const width = skillBar.getAttribute('data-width');
+                skillBar.style.width = width;
             }
         });
     };
+
+    skills.forEach(skill => {
+        // Animación de sombra y rotación basada en la posición del cursor
+        skill.addEventListener('mousemove', (e) => {
+            let width = skill.offsetWidth;
+            let height = skill.offsetHeight;
+
+            // Calcula la posición del cursor respecto al centro del elemento
+            let offsetX = 0.5 - (e.offsetX / width);
+            let offsetY = 0.5 - (e.offsetY / height);
+
+            // Usa esas coordenadas para aplicar una transformación y sombra
+            skill.style.transform = `rotateY(${offsetX * 40}deg) rotateX(${offsetY * 40}deg)`;
+            skill.style.boxShadow = `${-offsetX * 30}px ${offsetY * 30}px 15px rgba(0, 0, 0, 0.3)`;
+        });
+
+        // Cuando el cursor deja la tarjeta, restablece el estilo
+        skill.addEventListener('mouseleave', (e) => {
+            skill.style.transform = '';
+            skill.style.boxShadow = '';
+        });
+    });
 
     window.addEventListener('scroll', checkScroll);
     checkScroll();
