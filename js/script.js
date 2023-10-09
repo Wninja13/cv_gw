13c6d062b304b081aa3d65d870c58a83d60f49e1
GGW <german.weckesser@gmail.com>
2023-10-09 15:46:38 -0300

    Tarjetas certificados, ajuste de fuente

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const skillsBars = document.querySelectorAll('.skill-bar .progress');
-    const skills = document.querySelectorAll('.skill');
 
     const checkScroll = function() {
         skillsBars.forEach(skillBar => {
@@ -14,28 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     };
 
-    skills.forEach(skill => {
-        // Animación de sombra y rotación basada en la posición del cursor
-        skill.addEventListener('mousemove', (e) => {
-            let width = skill.offsetWidth;
-            let height = skill.offsetHeight;
-
-            // Calcula la posición del cursor respecto al centro del elemento
-            let offsetX = 0.5 - (e.offsetX / width);
-            let offsetY = 0.5 - (e.offsetY / height);
-
-            // Usa esas coordenadas para aplicar una transformación y sombra
-            skill.style.transform = `rotateY(${offsetX * 40}deg) rotateX(${offsetY * 40}deg)`;
-            skill.style.boxShadow = `${-offsetX * 30}px ${offsetY * 30}px 15px rgba(0, 0, 0, 0.3)`;
-        });
-
-        // Cuando el cursor deja la tarjeta, restablece el estilo
-        skill.addEventListener('mouseleave', (e) => {
-            skill.style.transform = '';
-            skill.style.boxShadow = '';
-        });
-    });
-
+    // Asígnalo al evento scroll y también llámalo inicialmente para cubrir cualquier barra que pueda estar visible al cargar la página
     window.addEventListener('scroll', checkScroll);
     checkScroll();
 });
