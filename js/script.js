dfc24f125fc6e0b90265019b50b673c548247173
German Weckesser <german.weckesser@gmail.com>
2023-10-11 12:58:37 -0300

    Agregación animación numeros barras

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
     const skillsBars = document.querySelectorAll('.skill-bar .progress');
 
+    const animateNumber = (element, start, end, duration) => {
+        let startTimestamp = null;
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            element.textContent = Math.floor(progress * (end - start) + start) + '%';
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    }
+
     const checkScroll = function() {
         skillsBars.forEach(skillBar => {
             const position = skillBar.getBoundingClientRect().top;
             const screenHeight = window.innerHeight;
 
+            // Revisa si la barra ya ha sido animada
+            if (skillBar.classList.contains('animated')) return;
+
             if (position <= screenHeight) {
                 const width = skillBar.getAttribute('data-width');
                 skillBar.style.width = width;
+
+                // Anima el número
+                const percentageElement = skillBar.querySelector('.percentage');
+                animateNumber(percentageElement, 0, parseInt(width), 2000);
+
+                // Marca la barra como animada
+                skillBar.classList.add('animated');
             }
         });
     };
@@ -17,6 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', checkScroll);
     checkScroll();
 });
+
 
 document.querySelectorAll('.social-icon').forEach(icon => {
     icon.addEventListener('mouseover', function() {
